aaf9bbae6de43db70c656aeb5327db92ce49cb84
Victor <hello@victordias.dev>
2022-04-09 21:32:00 -0300

    adding rich ansi output and constants

--- example/example.go
+++ example/example.go
@@ -7,16 +7,18 @@ import (
 )
 
 func main() {
-	log, err := logger.New(true, true, false, os.Stdout, "2006-01-02 15:04:05")
+	log, err := logger.New(true, true, false, true, os.Stdout, "2006-01-02 15:04:05")
 	if err != nil {
 		panic("Cannot initialize logger")
 	}
+
 	log.Info("This is an info message")
 	log.Success("This is a success message")
 	log.Warning("This is a warning message")
 	log.Error("This is an error message")
 	log.Critical("This is a critical message")
 	log.Debug("This is a debug message")
+	log.CustomAnsiPrint("This is a custom message print in dark purple", 89)
 
 	log.Infof("This is an info message %s", "with a string")
 	log.Successf("This is a success message %s", "with a string")
@@ -24,22 +26,32 @@ func main() {
 	log.Errorf("This is an error message %s", "with a string")
 	log.Criticalf("This is a critical message %s", "with a string")
 	log.Debugf("This is a debug message %s", "with a string")
+	log.CustomAnsiPrintf("This is a custom message print in dark purple %s", 89, "with a string")
 
-	logStack, err := logger.New(true, true, true, os.Stdout, "2006-01-02 15:04:05")
-	if err != nil {
-		panic("Cannot initialize logger")
-	}
-	logStack.Info("This is an info message with stack")
-	logStack.Success("This is a success message with stack")
-	logStack.Warning("This is a warning message with stack")
-	logStack.Error("This is an error message with stack")
-	logStack.Critical("This is a critical message with stack")
-	logStack.Debug("This is a debug message with stack")
+	log.SetShouldStack(true)
+	log.SetTimeFormat("02/01/2006 15:04:05")
 
-	logStack.Infof("This is an info message %s", "with a string and stack")
-	logStack.Successf("This is a success message %s", "with a string and stack")
-	logStack.Warningf("This is a warning message %s", "with a string and stack")
-	logStack.Errorf("This is an error message %s", "with a string and stack")
-	logStack.Criticalf("This is a critical message %s", "with a string and stack")
-	logStack.Debugf("This is a debug message %s", "with a string and stack")
+	log.Info("This is an info message with stack")
+	log.Success("This is a success message with stack")
+	log.Warning("This is a warning message with stack")
+	log.Error("This is an error message with stack")
+	log.Critical("This is a critical message with stack")
+	log.Debug("This is a debug message with stack")
+
+	log.Infof("This is an info message %s", "with a string and stack")
+	log.Successf("This is a success message %s", "with a string and stack")
+	log.Warningf("This is a warning message %s", "with a string and stack")
+	log.Errorf("This is an error message %s", "with a string and stack")
+	log.Criticalf("This is a critical message %s", "with a string and stack")
+	log.Debugf("This is a debug message %s", "with a string and stack")
+
+	log.SetRichOutput(false)
+
+	log.CustomAnsiPrint("This is a custom message print in itallic", logger.Italic)
+	log.CustomAnsiPrint("This is a custom message print in faint", logger.Faint)
+	log.CustomAnsiPrint("This is a custom message print in bold", logger.Bold)
+	log.CustomAnsiPrint("This is a custom message print in reverse", logger.ReverseVideo)
+	log.CustomAnsiPrint("This is a custom message print in underline", logger.Underline)
+	log.CustomAnsiPrint("This is a custom message print in crossed out", logger.CrossedOut)
+
 }
